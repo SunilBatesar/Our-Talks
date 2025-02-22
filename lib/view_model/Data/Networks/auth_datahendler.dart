b1e571c58e4e5ce1ad80d9e68448b771a3a99384
Naveen <kirmara09@gmail.com>
2025-02-22 12:59:27 +0530

    not change

--- lib/view_model/Data/Networks/auth_datahendler.dart
+++ lib/view_model/Data/Networks/auth_datahendler.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:ourtalks/view_model/Controllers/user_controller.dart';
+import 'package:ourtalks/Controllers/user_controller.dart';
 import 'package:ourtalks/view_model/Data/Networks/auth_function.dart';
 import 'package:ourtalks/view_model/Models/user_model.dart';
 
 class AuthDataHandler {
+  static final _repo = AuthRepository();
+  static final _userController = Get.find<UserController>();
   static Future<void> signUp(
       {required UserModel user, required String password}) async {
-    final repo = AuthRepository();
-    final userController = Get.find<UserController>();
-
     try {
-      final userSnapshot = await repo.signup(user: user, password: password);
-      userController.setUser(userSnapshot);
+      final userSnapshot = await _repo.signup(user: user, password: password);
+      _userController.setUser(userSnapshot);
+
       debugPrint("User signed up successfully: ${userSnapshot.toJson()}");
     } catch (e) {
       debugPrint("Error during sign up: $e");
@@ -21,12 +21,10 @@ class AuthDataHandler {
 
   static Future<void> login(
       {required String email, required String password}) async {
-    final repo = AuthRepository();
-    final userController = Get.find<UserController>();
-
     try {
-      final userSnapshot = await repo.login(email: email, password: password);
-      userController.setUser(userSnapshot);
+      final userSnapshot = await _repo.login(email: email, password: password);
+      _userController.setUser(userSnapshot);
+
       debugPrint("User logged in successfully: ${userSnapshot.toJson()}");
     } catch (e) {
       debugPrint("Error during login: $e");
