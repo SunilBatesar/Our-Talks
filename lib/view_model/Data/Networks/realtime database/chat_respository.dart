45c1c9b60451a8536fb836ce3436626fd0f63484
Naveen <kirmara09@gmail.com>
2025-03-07 13:34:23 +0530

    add the msd status funclaty and make function of send delever or read

--- lib/view_model/Data/Networks/realtime database/chat_respository.dart
+++ lib/view_model/Data/Networks/realtime database/chat_respository.dart
@@ -1,14 +1,16 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
+import 'package:get/get.dart';
 import 'package:ourtalks/view_model/Apis/firebase_apis.dart';
+import 'package:ourtalks/view_model/Controllers/user_controller.dart';
 import 'package:uuid/uuid.dart';
 
 class ChatRespository {
-  static final FirebaseAuth _auth = FirebaseAuth.instance;
-  static final User _currentUser = _auth.currentUser!;
+  // static final FirebaseAuth _auth = FirebaseAuth.instance
+  static final _usercontroller = Get.find<UserController>();
+  static final String _currentUser = _usercontroller.user!.userID!;
 
   static final FirebaseDatabase _firebaseDatabase = FirebaseDatabase.instance;
   static final _event = _firebaseDatabase.ref("chats");
@@ -18,7 +20,7 @@ class ChatRespository {
   static DatabaseReference getConversationID(
       String otherUserId, String folder) {
     // Sort user IDs lexicographically for consistent room IDs
-    final sortedIds = [_currentUser.uid, otherUserId]..sort();
+    final sortedIds = [_currentUser, otherUserId]..sort();
     return _event.child("${sortedIds[0]}_${sortedIds[1]}/$folder");
   }
 
@@ -26,6 +28,7 @@ class ChatRespository {
   static Future<void> sendMessage({
     required String text,
     required String receiverId,
+    required Map<String, dynamic> metadata,
   }) async {
     try {
       // Create proper message reference
@@ -33,10 +36,11 @@ class ChatRespository {
 
       // Create new message
       final newMessage = types.TextMessage(
-        author: types.User(id: _currentUser.uid),
+        author: types.User(id: _currentUser),
         createdAt: DateTime.now().millisecondsSinceEpoch,
         id: const Uuid().v4(),
         text: text,
+        metadata: metadata,
         // type: "text",
       ).toJson();
 
@@ -52,10 +56,9 @@ class ChatRespository {
 
   static Future<List<DocumentSnapshot>> getMyChatroomUser() async {
     try {
-      final chatroomSnapshot =
-          await FirebaseApis.userDocumentRef(_currentUser.uid)
-              .collection("my_chatroom")
-              .get();
+      final chatroomSnapshot = await FirebaseApis.userDocumentRef(_currentUser)
+          .collection("my_chatroom")
+          .get();
 
       final usersIDs = chatroomSnapshot.docs.map((doc) => doc.id).toList();
 
@@ -76,16 +79,17 @@ class ChatRespository {
   static Future<void> sendFirstMessage({
     required String text,
     required String receiverId,
+    required Map<String, dynamic> metadata,
   }) async {
     try {
-      final userDoc = await FirebaseApis.userDocumentRef(_currentUser.uid)
+      final userDoc = await FirebaseApis.userDocumentRef(_currentUser)
           .collection("my_chatroom")
           .doc(receiverId)
           .get();
 
       if (!userDoc.exists) {
         // Add receiver to the current user's "my_chatroom"
-        FirebaseApis.userDocumentRef(_currentUser.uid)
+        FirebaseApis.userDocumentRef(_currentUser)
             .collection("my_chatroom")
             .doc(receiverId)
             .set({});
@@ -93,14 +97,31 @@ class ChatRespository {
         // Also add current user to receiver's "my_chatroom"
         FirebaseApis.userDocumentRef(receiverId)
             .collection("my_chatroom")
-            .doc(_currentUser.uid)
+            .doc(_currentUser)
             .set({});
       }
 
       // Now send the message
-      await sendMessage(text: text, receiverId: receiverId);
+      await sendMessage(text: text, receiverId: receiverId, metadata: metadata);
     } catch (e) {
       debugPrint("Error sending first message: $e");
+      rethrow;
+    }
+  }
+
+  static Future<void> updateMessageStatus({
+    required String receiverId,
+    required String messageId,
+    required Map<String, dynamic> metadata,
+  }) async {
+    try {
+      final messageRef = getConversationID(receiverId, 'messages')
+          .child(messageId)
+          .child('metadata');
+
+      await messageRef.update(metadata);
+    } catch (e) {
+      debugPrint('Error updating message status: $e');
       rethrow;
     }
   }
