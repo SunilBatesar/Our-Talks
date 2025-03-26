1852f1b765a65266b6df3c555b4333a8a83a362f
Naveen <kirmara09@gmail.com>
2025-03-26 19:29:25 +0530

    solve sone issue

--- lib/Res/Services/app_config.dart
+++ lib/Res/Services/app_config.dart
@@ -2,7 +2,8 @@ class AppConfig {
   static String appName = "Our Talks"; // APP NAME
   static String appLogo = ""; // APP LOGO
   static String developedBy = "Sunil Batesar and Naveen"; // APP DEVELOPED BY
-  static String feedbackSendEmail = "sunil@gmail.com"; // FEEDBACK SEND EMAIL
+  static String feedbackSendEmail =
+      "kirmara09@gmail.com"; // FEEDBACK SEND EMAIL
   static String whatsAppContactNumber = "+919466051616"; // WHATSAPP NUMBER
   static String appPlayStoreLink =
       "https://play.google.com/store/apps/details?id=YOUR_PACKAGE_NAME"; // playStore link
